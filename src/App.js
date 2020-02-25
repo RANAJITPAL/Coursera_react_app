15fa554518b06d134a30a5a75d85dc112ce1eb85
Ranajit Pal <palranajit98@gmail.com>
2020-02-26 00:25:05 +0530

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 import Menu from "./components/MenuComponents";
+
 import {DISHES} from "./shared/dishes";
+
 
 class App extends Component{
 
--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-// import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
   CardTitle } from 'reactstrap';
+import DishDetail from './DishdetailComponent';
 
 class Menu extends Component {
 
@@ -17,22 +17,7 @@ class Menu extends Component {
       this.setState({ selectedDish: dish});
   }
 
-  renderDish(dish) {
-      if (dish != null)
-          return(
-              <Card>
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-              </Card>
-          );
-      else
-          return(
-              <div></div>
-          );
-  }
+  
 
   render() {
       const menu = this.props.dishes.map((dish) => {
@@ -56,7 +41,7 @@ class Menu extends Component {
               </div>
               <div className="row">
                 <div  className="col-12 col-md-5 m-1">
-                  {this.renderDish(this.state.selectedDish)}
+                 <DishDetail dish = {this.state.selectedDish}/>
                 </div>
               </div>
           </div>
